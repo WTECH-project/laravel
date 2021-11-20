64ea4d424855ba57429db487c6dd9b110347909b
Adam Bublavy <bublavy.adam@gmail.com>
2021-11-20 15:26:59 +0100

    Fix: Navbar responsivity

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,10 +6,11 @@
     <body class="flex flex-col min-h-screen">
         @include('layouts.partials.nav')
 
-        <main role="main" class="md:max-w-4xl w-full mx-auto flex-grow px-8 mt-12">
+        <main role="main" class="w-full mx-auto flex-grow px-8 mt-12">
             @yield('content')
         </main>
 
         @include('layouts.partials.footer-scripts')
+        @include('layouts.partials.footer')
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,25 +16,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index.index');
-});
+})->name('home');
 
 Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
 Route::post('/register', [RegisteredUserController::class, 'store']);
 
 Route::get('/settings', function() {
     return view('settings');
-});
+})->name('settings');
 
 Route::get('/checkout', function() {
     return view('checkout.index');
-});
+})->name('checkout');
 
 Route::get('/categories', function () {
     return view('products.index');
-});
+})->name('categories');
 
 Route::get('/product', function () {
     return view('products.show');
-});
+})->name('product');
 
 require __DIR__.'/auth.php';
