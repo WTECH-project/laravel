e4078d8ef6e74829b8738483e9ba6e023453aeed
Adam Bublavy <bublavy.adam@gmail.com>
2021-12-02 23:49:56 +0100

    Added: login and logout event listeners

---
+++ app/Listeners/LogSuccessfulLogout.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Logout;
+use Illuminate\Support\Facades\Log;
+
+class LogSuccessfulLogout
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Logout  $event
+     * @return void
+     */
+    public function handle(Logout $event)
+    {
+        Log::info('Pouzivatel sa prihlasil', ['id' => $event->user->id]);
+    }
+}
