8d3fe8e4cc161becda394e143e9a5f9615a8ab25
Adam Bublavy <bublavy.adam@gmail.com>
2021-12-05 00:55:03 +0100

    Fix: Admin home page

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,19 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="w-full flex-grow max-w-xl mx-auto pt-8 px-4 my-18 md:pt-12 sm:max-w-2xl md:max-w-3xl lg:flex-row lg:max-w-5xl lg:space-x-4 xl:max-w-7xl">   
+<section class="w-full flex-grow max-w-xl mx-auto pt-8 px-4 my-18 md:pt-12 sm:max-w-2xl md:max-w-3xl lg:flex-row lg:max-w-5xl lg:space-x-4 xl:max-w-7xl">
+    <h2 class="font-bold w-full text-3xl sm:text-4xl text-center mb-4">Zoznam produktov</h2>
     <div class="flex flex-col items-center">
-        <a class="border border-black p-2" href="{{ route('admin.showCreate') }}">
+        <a class="bg-white border-2 border-gray-400 
+                    hover:border-black transition duration-200 w-full md:w-auto cursor-pointer px-4 py-2 text-center" href="{{ route('admin.showCreate') }}">
             + Pridať produkt
-        </a> 
+        </a>
         <div class="gap-8 text-sm sm:text-base pt-2">
             @foreach($products as $product)
-            <x-admin.product link="{{ route('admin.show', $product->id) }}" name="{{ $product->name }}" description="{{ $product->description }}" price="{{ $product->price }}" img="{{ asset('storage/' . $product->images->first()->image_path) }}" productId="{{ $product->id }}"/>
+            <x-admin.product link="{{ route('admin.show', $product->id) }}" name="{{ $product->name }}" description="{{ $product->description }}" price="{{ $product->price }}" img="{{ asset('storage/' . $product->images->first()->image_path) }}" productId="{{ $product->id }}" />
             @endforeach
         </div>
-        <span class="mt-4">
-            
-        </span>
     </div>
-</div>
+</section>
 @endsection
