f9327e07523e3edc3e403521a723d35774102f9e
Adam Bublavy <bublavy.adam@gmail.com>
2021-12-04 15:01:04 +0100

    Fix: head.blade.php

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -3,4 +3,4 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Eshop</title>
 
-<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<link href="/css/app.css" rel="stylesheet">
